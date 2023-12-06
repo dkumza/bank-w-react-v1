5b41dffb4961392d810d24c47adf2eebc88f0257
MuchaSsak <MuchaSsak.tech+git@gmail.com>
2023-12-06 22:03:48 +0100

    refactor: disable react/prop-types

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -2,51 +2,50 @@
 import { useState } from "react";
 import { EditUser } from "./EditUser";
 
-/* eslint-disable react/prop-types */
 export const Store = ({
-   users,
-   updateUsers,
-   setUpdateUsers,
-   edit,
-   setEdit,
-   remove,
-   setRemove,
-   setClear,
-   handleSortClick,
+  users,
+  updateUsers,
+  setUpdateUsers,
+  edit,
+  setEdit,
+  remove,
+  setRemove,
+  setClear,
+  handleSortClick,
 }) => {
-   return (
-      <div className="store-wrap-main ">
-         <div className="info flex justify-between items-center">
-            <h1 className="text-3xl text-gray-700 text-center mb-2">
-               All Accounts
-            </h1>
-            {users.length > 1 ? (
-               <button
-                  className=" rounded-full hover:bg-green-500 hover:text-white h-6 px-4 text-sm"
-                  onClick={handleSortClick}
-               >
-                  A-Z
-               </button>
-            ) : null}
-         </div>
-         <div className="store-wrap">
-            {users === null && <p className="">Loading...</p>}
-            {users !== null && !users.length && (
-               <p className="m-2  text-base">Fill Data First</p>
-            )}
-            {users.map((user) => (
-               <EditUser
-                  key={user.id}
-                  user={user}
-                  setUpdateUsers={setUpdateUsers}
-                  edit={edit}
-                  setEdit={setEdit}
-                  remove={remove}
-                  setRemove={setRemove}
-                  setClear={setClear}
-               />
-            ))}
-         </div>
+  return (
+    <div className="store-wrap-main ">
+      <div className="info flex justify-between items-center">
+        <h1 className="text-3xl text-gray-700 text-center mb-2">
+          All Accounts
+        </h1>
+        {users.length > 1 ? (
+          <button
+            className=" rounded-full hover:bg-green-500 hover:text-white h-6 px-4 text-sm"
+            onClick={handleSortClick}
+          >
+            A-Z
+          </button>
+        ) : null}
       </div>
-   );
+      <div className="store-wrap">
+        {users === null && <p className="">Loading...</p>}
+        {users !== null && !users.length && (
+          <p className="m-2  text-base">Fill Data First</p>
+        )}
+        {users.map((user) => (
+          <EditUser
+            key={user.id}
+            user={user}
+            setUpdateUsers={setUpdateUsers}
+            edit={edit}
+            setEdit={setEdit}
+            remove={remove}
+            setRemove={setRemove}
+            setClear={setClear}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
